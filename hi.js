fa0d6ee6689e98514c007e6b8e91a8cba93d3d0d
serebano <mr.serebano@gmail.com>
2023-02-23 04:20:14 +0200

    fix: removed ver file

--- hi.js
+++ hi.js
@@ -1,1 +1,22 @@
 console.log('hi')
+
+[
+    "semantic-release-plugin-update-version-in-files",
+    {
+        "files": [
+            "version.js",
+            "README.md"
+        ],
+        "placeholder": "0.0.0-automated"
+    }
+],
+    [
+        "@semantic-release/git",
+        {
+            "assets": [
+                "README.md",
+                "version.js"
+            ],
+            "message": "chore(release): update version ${nextRelease.version} [skip ci]"
+        }
+    ],
