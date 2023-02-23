9a89731d843df45ff6469109fdf986f07277c8a4
serebano <mr.serebano@gmail.com>
2023-02-23 07:36:55 +0200

    fix: version.js

--- version.js
+++ version.js
@@ -1,1 +1,7 @@
-export const VERSION = '0.0.0-automated'
+import { exists } from "./helpers.js"
+
+let version = 0
+if (await exists('.version'))
+    version = await Deno.readTextFile(".version")
+
+export const VERSION = version //'0.0.0-automated'
