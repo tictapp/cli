376781f8899e10a8bf6c6432dfa948e4c25c0c76
serebano <mr.serebano@gmail.com>
2023-02-23 15:11:36 +0200

    feat(env): add envs from cli

--- env.js
+++ env.js
@@ -1,15 +1,26 @@
 #!/usr/bin/env -S deno run --allow-read --allow-write --allow-env --allow-net --allow-run --no-check
 
 import { API as DenoAPI } from "./api_deno.js";
-import { load } from "https://deno.land/std@0.177.0/dotenv/mod.ts";
+import { load, parse, stringify } from "https://deno.land/std@0.177.0/dotenv/mod.ts";
 
 export default async function _env(args) {
 
     const functionName = args._.shift()
+    const _env_str = args._.join("\n")
+
+    const _env_file = args.reset ? '' : await Deno.readTextFile(`functions/${functionName}/.env`).catch(e => '')
+    const _env_ = `${_env_file}\n${_env_str}`
+    const _env_parsed = parse(_env_)
+    const _env_str_final = stringify(_env_parsed)
+
+    //console.log(_env_parsed, _env_str_final)
+
+    await Deno.writeTextFile(`functions/${functionName}/.env`, _env_str_final);
 
     const PROJECT_REF = Deno.env.get('PROJECT_REF')
     const DENO_DEPLOY_TOKEN = Deno.env.get("DENO_DEPLOY_TOKEN")
     const DENO_DEPLOY_PROJECT = `${PROJECT_REF}-${functionName}`
+    const FUNCTIONS_DOMAIN = Deno.env.get("FUNCTIONS_DOMAIN")
 
     const envVars = await load({
         envPath: `functions/${functionName}/.env`,
@@ -19,7 +30,13 @@ export default async function _env(args) {
 
     const denoAPI = DenoAPI.fromToken(DENO_DEPLOY_TOKEN)
     const denoProject = await denoAPI.getProject(DENO_DEPLOY_PROJECT);
-
+    //let cenvs = {}
+    denoProject.envVars.forEach(e => {
+        if (!envVars[e]) {
+            envVars[e] = null
+        }
+        //cenvs[e] = null
+    })
     if (denoProject === null) {
 
         console.log('denoProject doesnt exists')
@@ -31,7 +48,16 @@ export default async function _env(args) {
             body: envVars
         })
 
-        console.log(res.envVars)
+        console.log(`
+
+[Done]
+%c
+https://${PROJECT_REF}.${FUNCTIONS_DOMAIN}/${functionName}
+https://${PROJECT_REF}-${functionName}.${FUNCTIONS_DOMAIN}
+        
+        %c
+${_env_str_final}`, 'color: lime', 'background-color: #222;color:#999')
+        // console.log(envVars)
     }
 
 }
--- example2/functions/bar-bar/index.js
+++ example2/functions/bar-bar/index.js
@@ -3,24 +3,24 @@ import { serve } from "https://deno.land/std@0.168.0/http/server.ts"
 
 console.log("Welcome Function")
 
-serve(async (req) => {
+serve((req) => {
     console.log(req.url)
 
-    const options = {
-        method: 'GET',
-        headers: { Authorization: 'Bearer ddp_ebahKKeZqiZVeOad7KJRHskLeP79Lf0OJXlj' }
-    };
+    // const options = {
+    //     method: 'GET',
+    //     headers: { Authorization: 'Bearer ddp_ebahKKeZqiZVeOad7KJRHskLeP79Lf0OJXlj' }
+    // };
 
-    const res = await fetch('https://dash.deno.com/api/projects/a379eb86-5fd2-4690-9b01-caf793ed5f38/deployments/latest/logs', options)
-    // .then(response => response.json())
-    // .then(response => console.log(response))
-    // .catch(err => console.error(err));
+    // const res = await fetch('https://dash.deno.com/api/projects/a379eb86-5fd2-4690-9b01-caf793ed5f38/deployments/latest/logs', options)
+    // // .then(response => response.json())
+    // // .then(response => console.log(response))
+    // // .catch(err => console.error(err));
 
-    return new Response(res.body, {
-        headers: {
-            'Content-Type': 'text/event-stream',
-        },
-    })
+    // return new Response(res.body, {
+    //     headers: {
+    //         'Content-Type': 'text/event-stream',
+    //     },
+    // })
 
     const data = {
         fun: 'Bar Bar',
