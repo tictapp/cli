f688a1e0cff8e89cad6ae2d87ba2b4274e42713b
serebano <mr.serebano@gmail.com>
2023-03-27 06:32:18 +0300

    fix: cleanup envs

--- .env.4c2c60a0.sh
+++
@@ -1,5 +0,0 @@
-vercel env add PUBLIC_SUPABASE_REF production < .env.4c2c60a0;
-vercel env add PUBLIC_SUPABASE_STUDIO production < .env.4c2c60a0;
-vercel env add PUBLIC_SUPABASE_URL production < .env.4c2c60a0;
-vercel env add PUBLIC_SUPABASE_ANON_KEY production < .env.4c2c60a0;
-vercel env add SUPABASE_SERVICE_KEY production < .env.4c2c60a0;
--- .env.sh
+++
@@ -1,5 +0,0 @@
-vercel env add PUBLIC_SUPABASE_REF production < .env;
-vercel env add PUBLIC_SUPABASE_STUDIO production < .env;
-vercel env add PUBLIC_SUPABASE_URL production < .env;
-vercel env add PUBLIC_SUPABASE_ANON_KEY production < .env;
-vercel env add SUPABASE_SERVICE_KEY production < .env;
