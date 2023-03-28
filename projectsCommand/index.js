bbdd022d2b9e452b78605985227b3ad1b86e6981
serebano <mr.serebano@gmail.com>
2023-03-28 23:35:56 +0300

    refactor(projectsCommand): rename list.js to projectsList.js feat(projectsCommand): throw validation error if invalid command is passed fix(projectsCommand): fix list command to use projectsList instead of projectsList()
    feat(projectsCommand/list.js): remove projectsList command and its implementation.
    
    feat(projectsList.js): add command to list all tictapp projects the logged-in user can access.

--- projectsCommand/index.js
+++ projectsCommand/index.js
@@ -1,18 +1,23 @@
 import { Command } from "https://deno.land/x/cliffy@v0.25.7/command/mod.ts";
-import projectsList from "./list.js";
+import projectsList from "./projectsList.js";
 import projectsNew from "./new.js";
 import projectsInfoCommand from "./projectsInfoCommand.js";
 import projectsTypesCommand from "./projectsTypesCommand.js";
+import { ValidationError } from "../deps.js";
 
 export default new Command()
     .name("projects")
     .description("Manage tictapp projects")
     .arguments("[command]")
-    .action(function (...a) {
+    .action(function (_, command) {
+        if (!command)
+            return this.getCommand("list").parse([])
+        if (command)
+            throw new ValidationError(`Invalid command: ${command}`)
         this.showHelp();
         return;
     })
     .command('info', projectsInfoCommand())
-    .command('list', projectsList())
+    .command('list', projectsList)
     .command('new', projectsNew())
     .command('types', projectsTypesCommand())
